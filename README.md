903eb4ffa34b4fb71e995303344b0634263b44b1
Ritika-ttn <ritika.gautam@tothenew.com>
2021-03-04 13:15:14 +0530

    success

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ No need to add icons
 Solution: Style.js
 </div>
 
-![styling1](./Screenshots/Styling1.png)
-![styling2](./Screenshots/Styling2.png)
+![styling1](./src/assets/Styling1.png)
+![styling2](./src/assets/Styling2.png)
--- index.js
+++ index.js
@@ -7,5 +7,5 @@ import App from './App';
 import { name as appName } from './app.json';
 // import ComponentEx from './ComponentEx';
 //import CounterParent from './CounterParent';
-import Style from './Style';
+import Style from './src/components/Style';
 AppRegistry.registerComponent(appName, () => Style);
