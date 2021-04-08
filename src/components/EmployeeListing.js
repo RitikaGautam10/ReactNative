5b6ddce27a9c6ed23cfc9c103d3d6d21cddf8f49
Ritika-ttn <ritika.gautam@tothenew.com>
2021-04-08 11:26:26 +0530

    Success

--- src/components/EmployeeListing.js
+++ src/components/EmployeeListing.js
@@ -54,9 +54,6 @@ class EmployeeListing extends Component {
       this.state.designation,
       this.state.salary,
     );
-    this.setState({
-      isModalVisible: false,
-    });
   };
 
   sortEmployeesInAsc = () => {
@@ -137,7 +134,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: 10,
-    // backgroundColor: 'red',
   },
   textinput: {
     borderWidth: 1,
