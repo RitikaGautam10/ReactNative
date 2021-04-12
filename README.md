a0be4f6013c1b5676cc4ce605037195f9c10526b
Ritika-ttn <ritika.gautam@tothenew.com>
2021-04-12 22:36:16 +0530

    Success

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-<DIV align="center">FIREBASE SETUP<DIV>
+<DIV align="center">Social integration<DIV>
 
-Create a Firebase project using firebase console.
-
-Create Android and iOS projects.
-
-Download google services files for both iOS and Android.
-
-Integrate Firebase into your React-native Application
-
-Integrate Cloud Messaging(Push Notifications) into your React-Native Application
+Login With Google
 
 ![output](./src/assets/output.gif)
--- index.js
+++ index.js
@@ -5,10 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Setup from './src/components/Setup';
-import messaging from '@react-native-firebase/messaging';
 import GoogleSignIn from './src/components/GoogleSignIn';
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log(remoteMessage);
-});
+
 AppRegistry.registerComponent(appName, () => GoogleSignIn);
